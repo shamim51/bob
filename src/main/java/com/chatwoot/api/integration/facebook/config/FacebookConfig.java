6b8b1ed53a925c5fdbb071c33cc884aad74ccdee
shamim51 <shamimahmed30082@gmail.com>
2026-09-19 19:11:35 +0600

    fixed build failed issue

--- src/main/java/com/chatwoot/api/integration/facebook/config/FacebookConfig.java
+++ src/main/java/com/chatwoot/api/integration/facebook/config/FacebookConfig.java
@@ -1,9 +1,18 @@
 package com.chatwoot.api.integration.facebook.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 @EnableConfigurationProperties(FacebookProperties.class)
 public class FacebookConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(RestClient.Builder.class)
+    RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
 }
